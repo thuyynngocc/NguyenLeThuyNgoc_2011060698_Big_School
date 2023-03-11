d8d5b6a18a1f1fde78bcc125985d4e562dd0065b
nltn0 <nltn0@LAPTOP-88Q63Q44>
2023-03-11 22:23:30 +0700

    Add project files.

---
+++ Big_School/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using Big_School.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Big_School.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Big_School/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Big_School.Startup))]
+namespace Big_School
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
